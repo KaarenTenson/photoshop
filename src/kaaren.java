722e246e9bcad8327d75721c6e5d77b78299405d
KaarenTenson <tensonkaaren@gmail.com>
2024-03-18 16:27:24 +0200

    pixelate tehtud

--- src/kaaren.java
+++ src/kaaren.java
@@ -1,2 +1,25 @@
 public class kaaren {
+    public static int[][] pixelate(int[][] maatriks, int ratio){
+        int csum=1;
+        int dsum=1;
+        int c=0;
+        int d=0;
+        int[][] tagastatav=new int[maatriks.length][maatriks[0].length];
+        for (int i = 0; i < tagastatav.length; i++) {
+            for (int j = 0; j < tagastatav[0].length; j++) {
+                if(csum==ratio||csum==tagastatav[0].length-c){
+                    csum=1;
+                    c=j;
+                }
+                if(dsum==ratio||dsum==tagastatav[0].length-d){
+                    dsum=1;
+                    d=i;
+                }
+                tagastatav[i][j]=maatriks[d][c];
+                csum++;
+            }
+            dsum++;
+        }
+        return tagastatav;
+    }
 }
